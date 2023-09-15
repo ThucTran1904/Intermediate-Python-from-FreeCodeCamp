6657b1acd7ead2ae8c81880a5b1870af717358a2
thuy.le <thuc.tran200409@vnuk.edu.vn>
2023-09-15 14:20:08 +0700

    Add dict file

---
+++ dict.py
@@ -0,0 +1,12 @@
+Thukdict = {"name": "Thuk", 
+            'age':21,
+            'city': 'DaNang'}
+print(Thukdict)
+# Another way to create Thukdict
+newThukdict = dict(name='ThukDepTrai', age=21,city=None)
+print(newThukdict)
+
+# Access the value in dic
+value = Thukdict['age']
+# age_value = newThukdict[age]
+print(value)
